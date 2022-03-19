9c127b952a34ce17f9bd4afe88c49cc0ab0b64ab
Pawel Dudek <paweldudek@MacBook-Pro.local>
2022-03-19 14:43:09 +0100

    Added entities

---
+++ src/main/java/com/cookieit/cart/domain/entity/Cart.java
@@ -0,0 +1,49 @@
+package com.cookieit.cart.domain.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+public class Cart extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String shopName;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private OffsetDateTime created;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "cart", cascade = {CascadeType.REMOVE})
+    private List<Item> items;
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public OffsetDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(OffsetDateTime created) {
+        this.created = created;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/cookieit/cart/domain/entity/Item.java
@@ -0,0 +1,45 @@
+package com.cookieit.cart.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Item extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cartId", nullable = false)
+    private Cart cart;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+}
