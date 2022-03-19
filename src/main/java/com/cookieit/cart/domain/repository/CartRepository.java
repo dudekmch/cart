1ccdebcb7e44a788763624914eb9403763c44908
Pawel Dudek <paweldudek@MacBook-Pro.local>
2022-03-19 14:47:15 +0100

    Added repositories

---
+++ src/main/java/com/cookieit/cart/domain/repository/CartRepository.java
@@ -0,0 +1,6 @@
+package com.cookieit.cart.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
---
+++ src/main/java/com/cookieit/cart/domain/repository/ItemRepository.java
@@ -0,0 +1,6 @@
+package com.cookieit.cart.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
