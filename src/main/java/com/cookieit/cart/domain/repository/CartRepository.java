f8fa6fd4aa4036800f8281e31abb6d3341e72b83
Pawel Dudek <paweldudek@MacBook-Pro.local>
2022-03-19 15:24:13 +0100

    Added @Transaclionat for repositories

--- src/main/java/com/cookieit/cart/domain/repository/CartRepository.java
+++ src/main/java/com/cookieit/cart/domain/repository/CartRepository.java
@@ -1,6 +1,10 @@
 package com.cookieit.cart.domain.repository;
 
+import com.cookieit.cart.domain.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface CartRepository extends JpaRepository<Cart, Long> {
 }
--- src/main/java/com/cookieit/cart/domain/repository/ItemRepository.java
+++ src/main/java/com/cookieit/cart/domain/repository/ItemRepository.java
@@ -1,6 +1,10 @@
 package com.cookieit.cart.domain.repository;
 
+import com.cookieit.cart.domain.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
