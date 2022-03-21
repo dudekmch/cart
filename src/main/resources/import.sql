ca68c9c89e9a7d26d94e8e5af30ced60a2a18bcf
Pawel Dudek <paweldudek@MacBook-Pro.local>
2022-03-21 23:42:28 +0100

    Change column cart.changed type to timestamp

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO cart.cart (id, shop_name) VALUES (1, 'shopName');
+INSERT INTO cart.cart (id, created, shop_name) VALUES (1, CURRENT_TIMESTAMP(), 'shopName');
 INSERT INTO cart.item (id, name, quantity, cart_id) VALUES (1, 'nameItemOne', 2, 1);
 INSERT INTO cart.item (id, name, quantity, cart_id) VALUES (2, 'nameItemTwo', 3, 1);
